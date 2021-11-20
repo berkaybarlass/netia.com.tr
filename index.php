5259533fc2a69fdbe713676a551e051a5d2256d4
Berkay Barlas <74761090+berkaybarlass@users.noreply.github.com>
2021-11-21 00:56:25 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE HTML>
+<!--
+Design By 	
+Berkay Barlas Frontend-Web Developer
+-->
+<html lang="tr">
+
+<meta name="description" content="Netia Tasarım Sağlık Çözümleri ">
+<meta name="keywords" content="Netia,Tasarım,Sağlık,Çözümleri,AresAir,Tecres,Lipogems">
+
+<head>
+
+	<title>Netia Tasarım & Sağlık Çözümleri </title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body class="homepage is-preload">
+	<div id="page-wrapper">
+
+		<!-- Header -->
+		<section id="header">
+			<div class="container">
+
+				<!-- Logo -->
+				<a id="logo"><a href="index.html"><img src="images/netiatr.jpg" alt="Netia" /></a>
+					<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li><a class="icon solid fa-home" href="index.php"><span>TR</span></a></li>
+							<li>
+							<li><a class="icon solid fa-home" href="index_en.php"><span>EN</span></a></li>
+						</ul>
+					</nav>
+
+			</div>
+		</section>
+
+		<!-- Features -->
+		<section id="features">
+			<div class="container">
+				<div class="row aln-center">
+					<div class="col-4 col-6-medium col-12-small">
+
+						<!-- Feature -->
+						<section>
+							<a target="_blank" href="https://aresair.com/index_tr.php" class="image featured"><img
+									src="images/aresairlogo.jpg" alt="Netia-Aresair" /></a>
+							<header>
+								<h2><a target="_blank" href="https://aresair.com/index_tr.php">Aresair</a></h2>
+							</header>
+							<p><a href="https://aresair.com/index_tr.php"> AresAir </a> <strong>Cerrahi Koruma Sistemi</strong>
+								hasta, cerrah ve ameliyathane personelini
+								enfeksiyon riski taşıyan vücut sıvıları, kan ve mikroorganizmaların transferine karşı
+								yüksek düzeyde koruma sağlamak amacıyla tasarlanmıştır.Sistem cerrahi kask ve tek
+								kullanımlık steril başlıktan oluşmaktadır.<a href="https://aresair.com/index_tr.php"> AresAir
+								</a>Cerrahi Kask, malzeme ve üretim
+								süreçleri açısından benzersiz bir üründür ve sağlık çalışanları için ekonomik,
+								dayanıklı, rahat ve pratik bir çözüm sunmaktadır.
+							</p>
+						</section>
+
+					</div>
+					<div class="col-4 col-6-medium col-12-small">
+
+						<!-- Feature -->
+						<section>
+							<a target="_blank" href="https://www.lipogems.com/tr" class="image featured"><img
+									src="images/lipogemslogo.jpg" alt="Netia-Lipogems" /></a>
+							<header>
+								<h2><a target="_blank" href="https://www.lipogems.com/tr">LIPOGEMS </a></h2>
+							</header>
+							<p><a target="_blank" href="https://www.lipogems.com/tr">Lipogems®</a> otolog adipoz dokuyu
+								kapalı bir sistemde tek bir cerrahi adımda işleyen, tek kullanımlık, steril medikal bir
+								cihazdır. İşlem kapalı bir sistemde tek bir cerrahi adımda gerçekleşir. İşleme tabi olan
+								doku asgari düzeyde manipüle edildikten sonra hasarlı dokunun tamiri ve yenilenmesi için
+								kullanılır.<a target="_blank" href="https://www.lipogems.com/tr">Lipogems®</a>yaralı
+								veya hasarlı dokuların yeniden yapılandırılması ve
+								onarımında doğal iyileşme sürecine destek olmak üzere majör bir cerrahi müdahaleye
+								alternatif olabilecek minimal invazif bir işlem olarak veya bir cerrahi uygulama içinde
+								ek bir işlem olarak kullanılabilir.
+						</section>
+
+					</div>
+					<div class="col-4 col-6-medium col-12-small">
+
+						<!-- Feature -->
+						<section>
+							<a target="_blank" href="https://tecres.com/category/joint-infection"
+								class="image featured"><img src="images/tecreslogo.jpg" alt="Netia-Tecres" /></a>
+							<header>
+								<h2><a target="_blank" href="https://tecres.com/category/joint-infection">Tecres</h2>
+								</a>
+							</header>
+							<p><a target="_blank" href="https://tecres.com/category/joint-infection">TECRES </a><strong>
+									1996 yılında,</strong> protez enfeksiyonunun tedavisinde
+								kullanılan önceden şekillendirilmiş ortopedik spacer’ın ilk endüstriyel üretimini
+								gerçekleştirmiştir. Protez enfeksiyonu ortopedik cerrahide karşılaşılan en ciddi
+								komplikasyonlardan biridir ve tedavisi çok zordur. Son klinik çalışmalarda da
+								belirtildiği gibi diz ve kalça protez cerrahisini takiben ilk 2 yıl içinde %1,6 oranında
+								bir çeşit enfeksiyon gelişmektedir. Protez enfeksiyonlarının tedavisinde 2 basamaklı
+								revizyon uygulaması standart olarak kabul edilmiştir.
+							</p>
+						</section>
+
+				
+					
+				</div>
+			</div>
+		</section>
+
+
+		<!-- Footer -->
+		<section id="footer">
+			<div class="container">
+				<header>
+					<h2>Soru veya Yorumlarınız için <strong>Bize Ulaşın:</strong></h2>
+				</header>
+				<div class="row">
+					<div class="col-6 col-12-medium">
+						<section>
+							<form method="post" action="#">
+								<div class="row gtr-50">
+									<div class="col-6 col-12-small">
+										<input name="name" placeholder="Ad-Soyad" type="text" />
+									</div>
+									<div class="col-6 col-12-small">
+										<input name="email" placeholder="E-posta " type="text" />
+									</div>
+									<div class="col-12">
+										<textarea name="message" placeholder="Mesajınız"></textarea>
+									</div>
+									<div class="col-12">
+										<a href="mailto:netia@netia.com.tr" class="form-button-submit button icon solid fa-envelope">Gönder</a>
+									</div>
+								</div>
+							</form>
+						</section>
+					</div>
+					<div class="col-6 col-12-medium">
+						<section>
+							<p>Netia Tasarım ve Sağlık Çözümleri Limited Şirketi</p>
+							<div class="row">
+								<div class="col-12 col-12-small">
+									<ul class="icons">
+										<li class="icon solid fa-home">
+											H. Rıfat Paşa Mah. Perpa Ticaret Merkezi
+											A Blok No:1254 Kat:9 Şişli 34384<br/>
+											 <strong>İstanbul Türkiye</strong><br/>
+										</li>
+										<li class="icon solid fa-phone">
+											<a href="tel:90 543 210 58 72"> 0543 210 58 72</a>
+
+										</li>
+										<li class="icon solid fa-envelope">
+											<a href="mailto:netia@netia.com.tr">netia@netia.com.tr</a>
+										</li>
+									</ul>
+								</div>
+						</section>
+					</div>
+				</div>
+			</div>
+			<div id="copyright" class="container">
+				<ul class="links">
+					<li>&copy;1998 Netia Tasarım ve Sağlık Çözümleri Limited Şirketi
+						<br>Tüm hakları saklıdır.</li>
+				</ul>
+			</div>
+		</section>
+
+	</div>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.dropotron.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ index_en.php
@@ -0,0 +1,190 @@
+<!DOCTYPE HTML>
+<!--
+Design By 	
+Berkay Barlas Frontend-Web Developer
+-->
+<html lang="en">
+<meta name="description" content="Netia Design & Health Solutions ">
+<meta name="keywords" content="Netia,Tasarım,Sağlık,Çözümleri,AresAir,Tecres,Lipogems">
+
+<head>
+
+    <title>Netia Design & Health Solutions </title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body class="homepage is-preload">
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <section id="header">
+            <div class="container">
+
+                <!-- Logo -->
+                <a id="logo"><a href="index.html"><img src="images/netiaeng.jpg" width="400px" alt="Netia" /></a>
+                    <!-- Nav -->
+                    <nav id="nav">
+                        <ul>
+                            <li><a class="icon solid fa-home" href="index.php"><span>TR</span></a></li>
+                            <li>
+                            <li><a class="icon solid fa-home" href="index_en.php"><span>EN</span></a></li>
+                        </ul>
+                    </nav>
+
+            </div>
+        </section>
+
+        <!-- Features -->
+        <section id="features">
+            <div class="container">
+                <div class="row aln-center">
+                    <div class="col-4 col-6-medium col-12-small">
+
+                        <!-- Feature -->
+                        <section>
+                            <a target="_blank" href="https://www.aresair.com" class="image featured"><img
+                                    src="images/aresairlogo.jpg" alt="Netia-Aresair" /></a>
+                            <header>
+                                <h2><a target="_blank" href="https://www.aresair.com">Aresair</a></h2>
+                            </header>
+                            <p><a href="https://www.aresair.com"> AresAir </a> <strong>Surgical Protection
+                                    System</strong>
+                                is designed to provide high level of protection against contamination, exposure to
+                                infectious bodily fluids and transfer of microorganisms. The system is composed of a
+                                surgical helmet and sterile disposable surgical hood.<a href="https://www.aresair.com">
+                                    AresAir
+                                </a>Surgical Helmet is a unique product with respect to material and manufacturing
+                                processes to provide an economical, modular, durable and at the same time a very
+                                comfortable solution for the healthcare providers.
+
+                            </p>
+                        </section>
+
+                    </div>
+                    <div class="col-4 col-6-medium col-12-small">
+
+                        <!-- Feature -->
+                        <section>
+                            <a target="_blank" href="https://www.lipogems.com/en/" class="image featured"><img
+                                    src="images/lipogemslogo.jpg" alt="Netia-Lipogems" /></a>
+                            <header>
+                                <h2><a target="_blank" href="https://www.lipogems.com/en/">LIPOGEMS </a></h2>
+                            </header>
+                            <p> The <a target="_blank" href="https://www.lipogems.com/en/">Lipogems®</a> system is a
+                                sterile single-use medical device intended for the closed-loop processing and
+                                transferring of autologous adipose tissue in a single surgical step. The processing
+                                takes place in a closed- loop system in a single surgical step. The processed tissue is
+                                minimally manipulated and used to repair, restore or replace damaged tissue.<a
+                                    target="_blank" href="https://www.lipogems.com/en/">Lipogems®</a> can be used as an
+                                adjunct to surgery or as a minimally invasive procedure option in alternative to major
+                                surgery as an aid to the natural healing process in the support, reconstruction and
+                                repair of injured or damaged tissue.
+
+                        </section>
+
+                    </div>
+                    <div class="col-4 col-6-medium col-12-small">
+
+                        <!-- Feature -->
+                        <section>
+                            <a target="_blank" href="https://tecres.com/category/joint-infection"
+                                class="image featured"><img src="images/tecreslogo.jpg" alt="Netia-Tecres" /></a>
+                            <header>
+                                <h2><a target="_blank" href="https://tecres.com/category/joint-infection">Tecres</h2>
+                                </a>
+                            </header>
+                            <p><a target="_blank" href="https://tecres.com/category/joint-infection">Tecres introduced
+                                </a><strong>
+                                    in 1996,</strong> the first industrial made, preformed orthopaedic Spacer,
+                                designed to help treat prosthetic infections. Prosthetic infection is one of the most
+                                serious complications in orthopedic surgery and is very difficult to treat. The latest
+                                data available in literature indicate that in the first two years after implanting hip
+                                or knee prosthesis, 1,6% will encounter some form of infections. The two-stage revision
+                                process is considered the standard for the treatment of prosthetic infections.
+
+                            </p>
+                        </section>
+
+
+                    </div>
+                </div>
+        </section>
+
+
+        <!-- Footer -->
+        <section id="footer">
+            <div class="container">
+                <header>
+                    <h2>Question or Comment <strong>Get in Touch:</strong></h2>
+                </header>
+                <div class="row">
+                    <div class="col-6 col-12-medium">
+                        <section>
+                            <form method="post" action="#">
+                                <div class="row gtr-50">
+                                    <div class="col-6 col-12-small">
+                                        <input name="name" placeholder="Name Surname" type="text" />
+                                    </div>
+                                    <div class="col-6 col-12-small">
+                                        <input name="email" placeholder="Email " type="text" />
+                                    </div>
+                                    <div class="col-12">
+                                        <textarea name="message" placeholder="Your message"></textarea>
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="mailto:netia@netia.com.tr"
+                                            class="form-button-submit button icon solid fa-envelope">Gönder</a>
+                                    </div>
+                                </div>
+                            </form>
+                        </section>
+                    </div>
+                    <div class="col-6 col-12-medium">
+                        <section>
+                            <p>Netia Design and Health Solutions Ltd.</p>
+                            <div class="row">
+                                <div class="col-12 col-12-small">
+                                    <ul class="icons">
+                                        <li class="icon solid fa-home">
+                                            H. Rıfat Paşa Mah. Perpa Ticaret Merkezi
+                                            A Blok No:1254 Kat:9 Şişli 34384<br />
+                                            <strong>İstanbul Turkey</strong><br />
+                                        </li>
+                                        <li class="icon solid fa-phone">
+                                            <a href="tel:90 543 210 58 72"> 0543 210 58 72</a>
+
+                                        </li>
+                                        <li class="icon solid fa-envelope">
+                                            <a href="mailto:netia@netia.com.tr">netia@netia.com.tr</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+            <div id="copyright" class="container">
+                <ul class="links">
+                    <li>&copy;1998 Netia Design and Health Solutions Ltd.
+                        <br>                        All Rights Reserved.
+
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.dropotron.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
